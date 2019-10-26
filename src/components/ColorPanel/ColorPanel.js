f891ec3ff5a7159b7e992b8e9c77af49eed29b6a
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-10-25 20:38:18 -0400

    cleanup

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -5,7 +5,11 @@ import './ColorPanel.css';
 class ColorPanel extends Component {
     render() {
         return (
-            <div className="color-panel" >ColorPanel</div>
+            <div className="color-panel">
+                <button className="color-panel__btn">
+                    <i className="fa fa-plus fa-lg"></i>
+                </button>
+            </div>
         )
     }
 }
