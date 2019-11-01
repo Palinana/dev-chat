02727340ea268b437cacda70bb2fcaa4e65871fc
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-10-31 21:19:44 -0400

    added search field to header

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -8,10 +8,25 @@ class MessagesHeader extends Component {
         
         return (
             <div className="messages__header">
-                <div className="messages__header-name">{channelName}</div>
-                <div className="messages__header-stats">
-                    <div className="messages__header-stats--users">{numUniqueUsers}</div>
-                    <div className="messages__header-stats--messages">{channelMessages}</div>
+                <div className="messages__header-info">
+                    <div className="messages__header-info__name">{channelName}</div>
+                    <div className="messages__header-info__stats">
+                        <div className="messages__header-info__stats--users">{numUniqueUsers}</div>
+                        <div className="messages__header-info__stats--messages">{channelMessages}</div>
+                    </div>
+                </div>
+
+                <div className="messages__header__search">
+                    <form className="messages__header__search-form" >
+                        <input 
+                            type="text" 
+                            name="search"
+                            placeholder="Search" 
+                        />
+                        <button className="messages__header__search--btn" type="submit">
+                            <img alt="send-icon" className="messages__header__search--image" src={require('../../Assets/Images/search.svg')} />
+                        </button>
+                    </form>
                 </div>
             </div>
         )
