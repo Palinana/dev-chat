4c62c6981da9b400949d1d1027d16969a22e1d94
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-11-05 18:31:37 -0500

    added starred component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,6 +3,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel/UserPanel';
 import Channels from './Channels/Channels';
 import DirectMessages from './DirectMessages/DirectMessages';
+import Starred from './Starred/Starred';
 
 import './SidePanel.css';
 
@@ -14,6 +15,7 @@ class SidePanel extends Component {
             <div className="side-panel">
                 <UserPanel currentUser={currentUser}/>
                 <Channels currentUser={currentUser}/>
+                <Starred />
                 <DirectMessages currentUser={currentUser}/>
             </div>
         )
